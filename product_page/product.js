bb2970edbf582f2153359c01984a8edfc1eb10c1
CemilUysal <16050111006@ybu.edu.tr>
2021-07-17 17:50:18 +0300

    some changes

--- product_page/product.js
+++ product_page/product.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var commentsHtml = "";
-
+    var type;
     var ourRequest = new XMLHttpRequest();     
     ourRequest.open("GET","https://cemil-web.herokuapp.com/data/products-api.php");
     ourRequest.onload = function(){
@@ -11,10 +11,10 @@ $(document).ready(function(){
 
     function renderHtml(data){
         for(i = 0 ; i<data.length; i++){
-           commentsHtml += '<div class="card my-5 w-50 bg-warning mx-auto"><div class="card-body">'+
+           commentsHtml += ' <div class="card my-5 w-50 bg-warning mx-auto"><div class="card-body">'+
            '<img src="https://via.placeholder.com/150x150" alt="image" class="personal-images image'+i+ '"width="40%"'+
            '/><div class=" card comment w-50 float-end"> <div class="card-header">'+
-           '<h3 class="card-title "><a href="../detay_page/detay.html">' + data[i].name +'</a></h3></div>'+
+           '<h3 class="card-title "><a href="../detay_page/detay.html" class="link">' + data[i].name +'</a></h3></div>'+
            '<div class="card-body"><h4 class="card-title">'+data[i].price +
            '</h4></div></div></div></div></div>';
            
@@ -23,6 +23,12 @@ $(document).ready(function(){
         for(i = 0; i<data.length;i++){
             $(".image"+i).attr("src",data[i].imagePaths);
         }
+        $(".link").click(function(){
+            type = $(this).text();
+            alert(type);
+        });
     }
+    
+    $(".urun-name").text();
    
 });
